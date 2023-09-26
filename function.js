a6bb406f7ae5e2d6a8346e42e1b64658d8ddd0fd
PexeMartinez <144402785+PexeMartinez@users.noreply.github.com>
2023-09-25 22:19:06 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+document.getElementById("DATOS PERSONALES").onclick = function () {
+       document.getElementById("DATOS PERSONALES").innerHTML = "INGRESE A CONTINUACIÓN SUS DATOS PERSONALES"
+}
+document.getElementById("OBJETIVO PROFESIONAL").onclick = function () {
+    document.getElementById("OBJETIVO PROFESIONAL").innerHTML = "Haz una pequeña descripción de tu experiencia o conocimientos que sean afines al puesto"
+}
